f306fd15943316c299945c098f5cbeff68f873ff
rikkola <toni@rikkola.net>
2024-06-19 12:35:14 +0300

    First commit

---
+++ akrivis-quarkus-runtime/src/main/java/org/kie/akrivis/quarkus/model/Configuration.java
@@ -0,0 +1,4 @@
+package org.kie.akrivis.quarkus.model;
+
+public record Configuration(String name, String api, Output[] outputs) {
+}
---
+++ akrivis-quarkus-runtime/src/main/java/org/kie/akrivis/quarkus/model/Output.java
@@ -0,0 +1,4 @@
+package org.kie.akrivis.quarkus.model;
+
+public record Output(String name, String from) {
+}
